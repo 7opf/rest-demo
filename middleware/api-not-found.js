eee595079bfd8d1e39f73cbe140240aebf37966a
Severin Skillman <s.skillman@surrey.ac.uk>
2019-03-25 17:41:20 +0000

    typo

--- middleware/api-not-found.js
+++ middleware/api-not-found.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-    var msg = req.params.id === undefined ? 'Resource with ID=' + req.params.id : 'Endpoint ' + req.path;
+    var msg = !!req.params.id ? 'Resource with ID=' + req.params.id : 'Endpoint ' + req.path;
     var notFoundError = new Error(msg + ' does not exist.');
     notFoundError.name = 'NotFoundError';
     next(notFoundError);
